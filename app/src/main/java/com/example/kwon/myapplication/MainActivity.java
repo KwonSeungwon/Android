5cef843cc71ba15ee1cf137f8efef7744b662d00
KwonSeungwon <loveyoo0103@naver.com>
2017-05-20 03:00:47 +0900

    All new upload

---
+++ app/src/main/java/com/example/kwon/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kwon.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+        public void onClick(View view)
+    {
+        Intent intent = new Intent(this, cmain.class);
+        startActivity(intent);
+
+        finish();
+    }
+}
