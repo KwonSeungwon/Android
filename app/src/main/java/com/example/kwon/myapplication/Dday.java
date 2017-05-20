1060ad4ca04170c74f2f15c9e5caff357b0f5789
KwonSeungwon <loveyoo0103@naver.com>
2017-05-21 01:52:37 +0900

    second upload

---
+++ app/src/main/java/com/example/kwon/myapplication/Dday.java
@@ -0,0 +1,16 @@
+package com.example.kwon.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Dday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dday);
+    }
+}
--- app/src/main/java/com/example/kwon/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kwon/myapplication/MainActivity.java
@@ -4,21 +4,65 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-        public void onClick(View view)
-    {
-        Intent intent = new Intent(this, cmain.class);
-        startActivity(intent);
+        Button test = (Button) findViewById(R.id.clabtn);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent = new Intent(getApplicationContext(), cmain.class);
+                startActivity(Intent);
 
-        finish();
+                Button test = (Button) findViewById(R.id.ddaybtn);
+                test.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent Intent = new Intent(getApplicationContext(), Dday.class);
+                        startActivity(Intent);
+
+                        Button test = (Button) findViewById(R.id.phbtn);
+                        test.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent Intent = new Intent(getApplicationContext(), Phmain.class);
+                                startActivity(Intent);
+
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
